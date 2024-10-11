e9a742bead6c8bed0ac2941cf3b74bc0af78b766
InnoNodo <k.nosov@innopolis.university>
2024-10-11 17:29:39 +0300

    Added user class in server.py

--- server.py
+++ server.py
@@ -11,6 +11,27 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 server.bind((LOCALHOST, PORT))
 print("Server was started")
+
+
+class ClientThread(threading.Thread):
+    def __init__(self, clientAddress, clientsocket):
+        threading.Thread.__init__(self)
+        self.csocket = clientsocket
+        print("Новое подключение: ", clientAddress)
+
+    def run(self):
+        print("Подключение с клиента : ", clientAddress)
+
+        msg = ''
+        while True:
+            data = self.csocket.recv(4096)
+            msg = data.decode()
+            print(msg)
+
+            if msg == '':
+                print("Отключение")
+                break
+
 while True:
     server.listen(1)
     clientsock, clientAddress = server.accept()
