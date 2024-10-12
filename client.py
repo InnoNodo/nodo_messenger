048fc6bb317d90fb4663bcdd6c279d0ef478df1e
InnoNodo <k.nosov@innopolis.university>
2024-10-12 19:56:53 +0300

    Added opportunity to communicate between client and server

--- client.py
+++ client.py
@@ -8,18 +8,27 @@ PORT = Settings().server.PORT
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
-client.sendall(bytes("979879789", "UTF-8"))
+print("You succesfully connected to the server")
 
 def task():
     while True:
-        in_data =  client.recv(4096)
-        print("От сервера :" ,in_data.decode())
+        try:
+            in_data = client.recv(4096)
+            if not in_data:
+                print("Connection closed by the server.")
+                break
+            print("From server: ", in_data.decode())
+        except ConnectionError:
+            break
 
 def task2():
     while True:
         out_data = input()
-        client.sendall(bytes(out_data,'UTF-8'))
-        print("Отпаравлено :" + str(out_data))
+        if out_data == 'exit':
+            client.sendall(bytes(out_data, 'UTF-8'))
+            client.close()
+            break
+        client.sendall(bytes(out_data, 'UTF-8'))
 
 t1 = Thread(target=task2)
 t2 = Thread(target=task)
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import socket, threading
+import socket
+import threading
 
 from config import Settings
 from database import get_session_maker
@@ -16,26 +17,66 @@ session_maker = get_session_maker(settings.database.connection_string)
 server.bind((LOCALHOST, PORT))
 print("Server was started")
 
+is_running = True
+clients = []
+
 class ClientThread(threading.Thread):
     def __init__(self, clientsocket, clientaddress):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
+        clients.append(clientsocket)
         print("New connection: ", clientaddress)
 
     def run(self):
         msg = ''
-        while True:
-            data = self.csocket.recv(4096)
-            msg = data.decode()
-            print(msg)
+        try:
+            while True:
+                data = self.csocket.recv(4096)
+                msg = data.decode()
+                if not data:
+                    break
 
-            if msg == 'exit':
-                print("Unconnection")
+                if msg == 'exit':
+                    print("Client was unconnected")
+                    break
+                else:
+                    print(f"Client says: {msg}")
+        except ConnectionError:
+            print("Connection closed abruptly.")
+        finally:
+            clients.remove(self.csocket)
+            self.csocket.close()
+
+def broadcast_message():
+    while is_running:
+        try:
+            message = input()
+            if message.lower() == 'exit':
+                stop_server()
                 break
+            for client in clients:
+                try:
+                    client.sendall(bytes(message, 'UTF-8'))
+                except ConnectionError:
+                    clients.remove(client)
+        except KeyboardInterrupt:
+            stop_server()
+            break
 
-while True:
-    server.listen(1)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientsock, clientAddress)
-    newthread.run()
+def stop_server():
+    global is_running
+    is_running = False
+    server.close()
+    exit()
 
+admin_thread = threading.Thread(target=broadcast_message)
+admin_thread.start()
+
+while is_running:
+    try:
+        server.listen(1)
+        clientsock, clientAddress = server.accept()
+        newthread = ClientThread(clientsock, clientAddress)
+        newthread.start()
+    except OSError:
+        print("Server stopped listening.")
